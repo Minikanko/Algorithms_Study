9980259d977c8a9ad377d9d90ff2d0066bd30d4b
Minikanko <bcgrhio@naver.com>
2021-06-01 23:36:37 +0900

    2021-06-01 LinkedList_1158

--- src/BJ/Array_1406.java
+++ src/BJ/Array_1406.java
@@ -1,16 +1,25 @@
 package BJ;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Array_1406 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringBuilder sb = new StringBuilder(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		List<String> list = new LinkedList<String>();
+		String[] beforeStr = br.readLine().split("");
+		for(String s: beforeStr)
+			list.add(s);
+		
 		int M = Integer.parseInt(br.readLine());
-		int pointer = sb.length();
+		int pointer = list.size();
 
 		while (M-- > 0) {
 			String[] str = br.readLine().split(" ");
@@ -23,23 +32,31 @@ public class Array_1406 {
 				break;
 			//커서를 오른쪽으로 한 칸 옮김
 			case "D":
-				if (pointer < sb.length()) {
+				if (pointer < list.size()) {
 					pointer++;
 				}
 				break;
 			//커서 왼쪽에 있는 문자를 삭제함 (커서가 문장의 맨 앞이면 무시됨)
 			case "B":
 				if (pointer > 0) {
-					sb.deleteCharAt(--pointer);
+					list.remove(--pointer);
 				}
 				break;
 			//$라는 문자를 커서 왼쪽에 추가함
 			case "P":
-				sb.insert(pointer, str[1]);
+				list.add(pointer, str[1]);
 				pointer++;
 				break;
 			}
 		}
-		System.out.println(sb.toString());
+	list.forEach(x->{
+		try {
+			bw.write(x);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	});
+		bw.flush();
+		
 	}
 }
