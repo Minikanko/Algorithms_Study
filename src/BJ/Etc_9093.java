a3fce5a61f3d45f4a8d987862a0c487bf3d6a372
Minikanko <bcgrhio@naver.com>
2021-06-16 23:10:07 +0900

    2021-06-16 Stack_9093

--- src/BJ/Etc_9093.java
+++ src/BJ/Etc_9093.java
@@ -5,28 +5,36 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;import com.sun.jdi.Value;
+import java.util.Stack;
 
-public class Sort_2751 {
-
+public class Etc_9093 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
-		List<Integer> nums = new ArrayList<Integer>();
 		
-		for(int i=0;i<N;i++) {
-			nums.add(Integer.parseInt(br.readLine()));
-		}
-		
-		Collections.sort(nums);
-		for(int num:nums) {
-			bw.write(num+"\n");
+		while(N-- >0) {
+			String[] strs = br.readLine().split(" ");
+			Stack<Character> stack = new Stack<Character>();
+			
+			for(int i=0;i<strs.length;i++) {
+				for(char c : strs[i].toCharArray()) {
+					stack.push(c);
+				}
+				
+				while(!stack.isEmpty()) {
+					bw.write(stack.pop());
+				}
+				bw.write(" ");
+			}
+			bw.write("\n");
+			
 		}
 		bw.flush();
+		
+		
+	
+		
 	}
-
 }
