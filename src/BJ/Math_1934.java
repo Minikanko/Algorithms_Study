e4232595a4a99beae76a9294d8e1506a3718e9c4
Minikanko <bcgrhio@naver.com>
2021-06-21 22:31:06 +0900

    2021-06-21 Math_2609 유클리드 호제법 알고리즘 사용

---
+++ src/BJ/Math_1934.java
@@ -0,0 +1,37 @@
+package BJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Math_1934 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		while(N-- > 0) {
+			String[] str = br.readLine().split(" ");
+			int[] nums = new int[2];
+			
+			nums[0]= Integer.parseInt(str[0]);
+			nums[1]= Integer.parseInt(str[1]);
+			
+			int gcd = reculGcd(nums[0],nums[1]);
+			int lcm = nums[0]*nums[1]/gcd;
+			
+			System.out.println(lcm);
+		}
+		
+		
+	}
+	
+	public static int reculGcd(int num1,int num2) {
+		if(num2==0) return num1;
+		return reculGcd(num2,num1%num2);
+	}
+
+}
+
