a100da2bad91f7c21098db0edc0ee5206a790f6c
Minikanko <bcgrhio@naver.com>
2021-06-23 21:23:22 +0900

    2021-06-23 Math_1978 소수찾기 로직 수정

--- src/BJ/Math_1978.java
+++ src/BJ/Math_1978.java
@@ -10,6 +10,8 @@ public class Math_1978 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int count=0;
+		int N = Integer.parseInt(br.readLine());
+		
 		String[] str = br.readLine().split(" ");
 		for(String s : str) {
 			if(is_Prime(Integer.parseInt(s)))
@@ -19,11 +21,6 @@ public class Math_1978 {
 		
 	}
 	
-//	public static boolean cal(int num1, int index) {
-//		if(num1%index==0 && num1>index) 
-//			return false;
-//		return cal(num1,index++);
-//	}
 	public static boolean is_Prime(int Number) {
 		 
 		// 1 은 소수가 아니다.
@@ -31,12 +28,10 @@ public class Math_1978 {
 			return false;	
 		}
 	    
-	    
 		// 2 ~ Number-1 까지 중 나누어 떨어지는 약수가 있는지 판별
 		// Number = 2 의 경우는 자연스럽게 for문을 검사하지 않게 됨
 	    
 		for(int i = 2; i < Number; i++) {
-	    
 			if(Number % i == 0) return false;
 	        
 		}
